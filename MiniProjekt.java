3ae1cc4d28dfe217ffb23189843d70cc4c8a4d77
Omid200716 <112422234+Omid200716@users.noreply.github.com>
2022-11-04 20:06:55 +0100

    uppdaterat miniprojek
    uppdaterat projekt.

--- MiniProjekt.java
+++ MiniProjekt.java
@@ -1,94 +1,78 @@
-package miniprojekt;
+import java.util.Random;
+import java.util.Scanner;
 
- import java.util.Random;
- import java.util.Scanner;
+public class Main {
 
- public class MiniProjekt {
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		Scanner input = new Scanner(System.in);
+		
+		
+	int randomNummer;
+	int tryCoun;
+	
 
- 	public static void main(String[] args) {
+		
+			
+		while (true) {
+			tryCoun = 0;
+		    randomNummer = rand.nextInt(100)+1;
+		    System.out.println(randomNummer);
+		
+			System.out.println(" Guess from  0 to 100 ");
+			
+			while (true) {
+				
+				
+				
+				int answer = input.nextInt();
+				tryCoun++;
 
- 		
- 	int answer, guess;
- 	final int MAX = 100;
- 	int a =1;
- 	
- 	
-  boolean loop = false;
+				
+				if (checkAnswer(randomNummer, answer) == 0){
+					System.out.println("Corecct you win the game!");
+					System.out.println(" You tried " + tryCoun);
+					
+					break;
+					
+				} 
+				
+				if (checkAnswer(randomNummer, answer) ==1) {
+					System.out.println(" the number was bigger, try again!");
+				} 
+				
+				if (checkAnswer(randomNummer, answer) ==2) {
 
- 		
- 		Scanner sc = new Scanner(System.in);
- 		Random rand = new Random();
- 		
-		answer = rand.nextInt(MAX)+1;
- 		
- 		
- 		System.out.println("\n    Welcome to this game!");
- 		System.out.println("Guess a nummer between 0 to 100!!");
- 		
- 				
- 		System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
- 		
- 		
- 		do {
- 			
- 		
- 		guess = sc.nextInt();
+					System.out.println(" the number was smaller, try again! ");
+					
+					
+				}
+					
+}
+			
+		       }      
+		
+			
+		}
+	
+	public static int checkAnswer( int guess, int answer) {
+		if ( guess == answer)  {
+			
+			
+			return 0;
+			
+		}else if( answer < guess) {
+			return 1;
+		}else {
+			return 2;
+		}
+		
 
- 		
- 		if (guess > 100) {
- 			System.out.println("Just between 0 to 100 please!!");
- 			System.out.println(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-");
- 			
- 			
- 		}
- 		
- 		
+		
+		
 
- 		if ( checkAnswer(answer, guess) == 0 ) {
- 			System.out.println( " Good job man!!  the nummer was " + answer );
- 			System.out.println("You tried " + a);
- 			System.out.println("_____________________________________");
- 			
- 			loop = true;
+	}
 
- 		}
- 		
- 		
- 		 if(checkAnswer(answer, guess) == -1) {
- 			 System.out.println("and your number was bigger ");
- 			 System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_");
- 			System.out.println("Try agian");
- 			System.out.println("______________________________");
- 		}
- 		
- 		 if (checkAnswer(answer, guess) == 1) {
- 			 System.out.println("and your number was smaller");
- 			 System.out.println("_-_-_-_-_-_-_-_-_-_-_-__-_-");
- 			 System.out.println("Try agian");
- 				System.out.println("______________________________");
- 		 }
- 		
- 		 a++;
+}
 
- 	} while (!loop);
- 		
-
- 	
- 	}
- 	public static int checkAnswer (int answer, int guess) {
- 		if ( answer == guess) {
- 			return 0;
- 		} else if  ( answer < guess) {
- 			return -1;
- 		}
- 		else {
- 			return 1;
- 		}
- 			
- 	}
- 	
- 	
- 	
- 		
-
- }
